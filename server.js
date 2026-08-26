219580aaa4ca5479647312397e160f1cf11fd9ce
IT Engineer <cch1527097@outlook.com>
2026-08-26 14:30:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -176,7 +176,8 @@ async function initDatabase() {
             { id: 'store_51', name: '樂法', category: '喝涼涼', isOpen: false },
             { id: 'store_52', name: '一鳴', category: '美味餐點', isOpen: false },
             { id: 'store_53', name: '五路鍋聖', category: '美味餐點', isOpen: false },
-            { id: 'store_54', name: '秈鋤料理廚房', category: '美味餐點', isOpen: false }
+            { id: 'store_54', name: '秈鋤料理廚房', category: '美味餐點', isOpen: false },
+            { id: 'store_55', name: '江滬上海小籠湯包', category: '美味餐點', isOpen: false }
         ];
 
         const currentStoreRes = await pool.query("SELECT value FROM settings WHERE key = 'store_list';");
